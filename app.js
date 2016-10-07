3b6ec6bfd962462a69db9139db09e4e4ed0268f7
Juliane Blier <blier.juliane@gmail.com>
2016-10-07 11:10:00 +0200

    finalisation de l'exercice

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log('have fun !');
+
+var person = {};
+// Au moment du clic 
+
+$('button').click(function(){
+
+	var inputs = $('input');
+	console.log(inputs);
+
+	for (var i = 0; i < inputs.length; i++) {
+		person[$(inputs[i]).attr('name')] = $(inputs[i]).val();
+		
+	}
+	console.log(person);
+
+	greetings = person.first_name + ' ' + person.last_name + ' de ' + person.city;
+	console.log(greetings);
+
+	$('#username').text(greetings + ' !');
+});
